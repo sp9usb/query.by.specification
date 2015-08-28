d3116de1efe843c554d3f1039a107a08f3b140fe
Ryan Bartsch <rbartsch@homestart.com.au>
2015-08-28 11:41:32 +0930

    added generic specification repository to framework

--- query.by.specification/BaseSpecificationRepository.cs
+++ query.by.specification/BaseSpecificationRepository.cs
@@ -3,11 +3,6 @@ using System.Linq;
 
 namespace query.@by.specification
 {
-    public interface ISpecificationRepository<T>
-    {
-        IList<T> FindBy(ISpecification<T> specification);
-    }
-
     public abstract class BaseSpecificationRepository<T> : ISpecificationRepository<T>
     {
         public abstract IQueryable<T> Expandable { get; }
