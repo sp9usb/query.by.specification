e082c5239decd36469f8e9faa176211cdc44dc94
Ryan Bartsch <rbartsch@homestart.com.au>
2015-09-04 09:21:41 +0930

    added data contract attribute to base specification in DataContracts library

--- query.by.specification.DataContracts/Specification/BaseSpecification.cs
+++ query.by.specification.DataContracts/Specification/BaseSpecification.cs
@@ -1,5 +1,8 @@
+using System.Runtime.Serialization;
+
 namespace query.@by.specification.DataContracts.Specification
 {
+    [DataContract]
     public abstract class BaseSpecification<T>
     {
     }
